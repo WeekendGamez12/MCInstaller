c8f0d172d2cefe16b10d2968c4534cfb3fc1b87a
LunaCoder2008 <101733083+LunaCoder2008@users.noreply.github.com>
2022-03-25 12:02:45 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheArray = [
+
+  'assets.epk',
+  'classes.js',
+  'classes.js.map',
+  'index.html'
+];
+var cacheName='mcinstallercache';
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(cacheArray);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+  self.skipWaiting();
+});
